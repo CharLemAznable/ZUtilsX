1beed729cabf218e3b2b9d5bddb48a05c2755b28
CharLemAznable <545541819@qq.com>
2015-12-07 16:58:11 +0800

    Add NSCalendarUnit adapt, add NSDate properties.

--- ZUtilsX/Categories/Foundation/NSDate+ZUX.m
+++ ZUtilsX/Categories/Foundation/NSDate+ZUX.m
@@ -25,7 +25,27 @@ ZUX_CATEGORY_M(ZUX_NSDate)
     return [self timeIntervalSince1970] * 1000;
 }
 
+#define ZUXNSDateComponent_implement(calendarUnit, componentName) \
+- (NSInteger)componentName { \
+    return [[NSCalendar currentCalendar] components:calendarUnit fromDate:self].componentName; \
+}
+
+ZUXNSDateComponent_implement(NSEraCalendarUnit, era);
+ZUXNSDateComponent_implement(NSYearCalendarUnit, year);
+ZUXNSDateComponent_implement(NSMonthCalendarUnit, month);
+ZUXNSDateComponent_implement(NSDayCalendarUnit, day);
+ZUXNSDateComponent_implement(NSHourCalendarUnit, hour);
+ZUXNSDateComponent_implement(NSMinuteCalendarUnit, minute);
+ZUXNSDateComponent_implement(NSSecondCalendarUnit, second);
+ZUXNSDateComponent_implement(NSWeekdayCalendarUnit, weekday);
+ZUXNSDateComponent_implement(NSWeekdayOrdinalCalendarUnit, weekdayOrdinal);
+
+@end
+
+@implementation NSDate (ZUXStringDate)
+
 - (NSString *)stringWithDateFormat:(NSString *)dateFormat {
+    [[[NSCalendar currentCalendar] components:NSEraCalendarUnit fromDate:self] valueForKey:@"era"];
     NSDateFormatter *formatter = ZUX_AUTORELEASE([[NSDateFormatter alloc] init]);
     formatter.dateFormat = dateFormat;
     return [formatter stringFromDate:self];
@@ -33,7 +53,7 @@ ZUX_CATEGORY_M(ZUX_NSDate)
 
 @end
 
-@implementation NSString (ZUXDate)
+@implementation NSString (ZUXStringDate)
 
 - (NSDate *)dateWithDateFormat:(NSString *)dateFormat {
     NSDateFormatter *formatter = ZUX_AUTORELEASE([[NSDateFormatter alloc] init]);
