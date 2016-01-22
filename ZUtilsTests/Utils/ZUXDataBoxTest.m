ac904e354e984166c3e7e6ca292eed356c34ecf3
CharLemAznable <545541819@qq.com>
2016-01-22 18:05:54 +0800

    Modify test asserts.

--- ZUtilsTests/Utils/ZUXDataBoxTest.m
+++ ZUtilsTests/Utils/ZUXDataBoxTest.m
@@ -45,13 +45,15 @@
     [[UserDefaults shareUserDefaults] synchronize];
     
     [UserDefaults shareUserDefaults].userId = @"111";
+    [UserDefaults shareUserDefaults].version = nil;
     XCTAssertEqualObjects([UserDefaults shareUserDefaults].userId, @"111");
     XCTAssertEqualObjects([UserDefaults shareUserDefaults].name, @"aaa");
-    XCTAssertEqualObjects([UserDefaults shareUserDefaults].version, @"0.0.1");
+    XCTAssertNil([UserDefaults shareUserDefaults].version);
     
     [UserDefaults shareUserDefaults].userId = @"222";
+    [UserDefaults shareUserDefaults].name = nil;
     XCTAssertEqualObjects([UserDefaults shareUserDefaults].userId, @"222");
-    XCTAssertEqualObjects([UserDefaults shareUserDefaults].name, @"bbb");
+    XCTAssertNil([UserDefaults shareUserDefaults].name);
     XCTAssertEqualObjects([UserDefaults shareUserDefaults].version, @"0.0.2");
 }
 
